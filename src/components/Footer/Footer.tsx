fc61b3f383b29404f572a84c7a51621a95fd7458
adnan-the-dev <azizart320480@gmail.com>
2025-05-17 15:57:06 +0500

    final commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,4 @@
 import { Container } from "./styles";
-import reactIcon from "../../assets/react-icon.svg";
-import linkedin from "../../assets/linkedin.svg";
-import githubIcon from "../../assets/github.svg";
-import whatsapp from "../../assets/whatsapp.svg";
-import telegram from "../../assets/telegram.svg";
-import instagramIcon from "../../assets/instagram.svg";
 import logo from "../../assets2/llight-1.png";
 
 export const Footer: React.FC = () => {
--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import {
   Dialog,
   DialogContent,
-  IconButton,
   Box,
   Typography,
 } from "@mui/material";
